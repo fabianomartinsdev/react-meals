e769830618e0212147502155a901d2f00c8a3a68
Fabiano Martins <fabismaple01@gmail.com>
2021-09-01 14:02:17 -0300

    Create landing page with header, button and card

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import meals from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={meals} alt="Table full of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
